9a8b57c7baa26da4cda4a5138cab01d2ac6568a8
Peyvçîn Developer <peyvchin@example.com>
2026-07-14 08:22:33 +0300

    fix: forcefully manipulate DOM to eliminate iOS white gap

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 /* global importScripts */
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE_NAME = 'peyvchin-v17';
+const CACHE_NAME = 'peyvchin-v18';
 const ASSETS = [
   '/',
   '/index.html',
