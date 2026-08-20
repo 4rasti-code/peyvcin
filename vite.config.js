fd5cbce56b5c10dc2cc257a5e7293a8af0cb5280
Peyvçîn Developer <peyvchin@example.com>
2026-08-20 03:34:07 +0300

    chore: increase Vite chunk size warning limit to 3000

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    chunkSizeWarningLimit: 1500,
+    chunkSizeWarningLimit: 3000,
   },
   server: {
     port: 5173,
